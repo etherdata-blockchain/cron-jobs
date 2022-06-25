7697a8ac812ba860bf1dde6fcd87d9649538f68c
sirily11 <sirily1997@gmail.com>
2022-06-25 22:57:50 +0800

    BREAKING CHANGE: rewrite whole fetcher

--- block_fetcher/block_fetcher.py
+++ block_fetcher/block_fetcher.py
@@ -1,43 +1,51 @@
-import json
 import os
 import traceback
+import uuid
 from typing import Dict, List, Tuple
+
 import certifi
 import requests
 from pymongo import MongoClient
 from pymongo.collection import Collection
-import datetime
 from tqdm import tqdm
 
 
 class BlockFetcher:
     def __init__(self):
         self.url = os.getenv("RPC_URL")
-        self.db_endpoint = os.getenv("MONGODB_URL")
+        self.db_endpoint = os.getenv("DATABASE_URL")
         self.latest_db_block_number = 0
         self.latest_rpc_block_number = 0
         self.mongodb: MongoClient = None
-        self.keys_transform_ignored = ["parentHash", "hash", "sha3Uncles", "stateRoot", "blockHash", "from", "to",
-                                       "miner", "extraData", "minHash", "receiptsRoot", "transactionsRoot",  "nonce", "blockHash"]
-        self.keys_ignored = ["logsBloom", "r", "s", "v", "input"]
+        self.headers = {
+            'Content-Type': 'application/json'
+        }
 
     def __connect_db__(self):
         self.mongodb = MongoClient(self.db_endpoint, tlsCAFile=certifi.where())
+        collection = self.mongodb.etd.blocks
+        collection.create_index([("hash", 1)], unique=True)
 
     def __fetch_rpc_block_number__(self) -> int:
-        data = {"jsonrpc": "2.0", "method": "etd_blockNumber", "params": []}
-        resp = requests.post(self.url, json=data)
-        resp_data = resp.json()
+        data = {
+            "jsonrpc": "2.0",
+            "method": "eth_blockNumber",
+            "params": [],
+            "id": str(uuid.uuid4())
+        }
+
+        response = requests.request("POST", self.url, headers=self.headers, json=data)
+        resp_data = response.json()
         block_num_in_hex = resp_data['result']
         return int(block_num_in_hex, 16)
 
     def __fetch_db_block_number__(self) -> int:
         db = self.mongodb.etd
         blocks_col: Collection = db.blocks
-        highest_blocks = list(blocks_col.find({}).sort("number", -1).limit(1))
+        highest_blocks = list(blocks_col.find({}).sort("numberInBase10", -1).limit(1))
         if len(highest_blocks) == 0:
             return 0
-        return int(highest_blocks[0]['number'])
+        return int(highest_blocks[0]['numberInBase10'])
 
     def __fetch_block__(self, block_number: int) -> Tuple[List, List]:
         """
@@ -46,16 +54,14 @@ class BlockFetcher:
         :return: List of blocks and transactions. Blocks will be current block and its uncles
         """
         hex_block_number = hex(block_number)
-        data = {"jsonrpc": "2.0", "method": "etd_getBlockByNumber", "params": [hex_block_number, True]}
-        resp = requests.post(self.url, json=data)
+        data = {"jsonrpc": "2.0", "method": "eth_getBlockByNumber", "params": [hex_block_number, True],
+                "id": str(uuid.uuid4()), }
+        resp = requests.request("POST", self.url, json=data, headers=self.headers)
         block: Dict = resp.json()['result']
+        block["numberInBase10"] = int(block["number"], 16)
         blocks = []
-        block = self.__normalize_dict__(block)
         uncles = self.__fetch_uncles__(block['uncles'], block['hash'])
         transactions = block.pop("transactions")
-
-        for index, tx in enumerate(transactions):
-            transactions[index] = self.__normalize_dict__(tx)
 
         blocks.append(block)
         blocks += uncles
@@ -67,33 +73,12 @@ class BlockFetcher:
         block_col: Collection = db.blocks
         block_col.delete_many({})
 
-
-    def __normalize_dict__(self, data: Dict):
-        items = list(data.items())
-        for key, value in items:
-            if key in self.keys_ignored:
-                del data[key]
-                continue
-
-            if key in self.keys_transform_ignored:
-                continue
-
-            if key == "timestamp":
-                data[key] = datetime.datetime.fromtimestamp(int(value, 16))
-                continue
-
-            if type(value) == str and value.startswith("0x"):
-                data[key] = float(int(value, 16))
-
-        return data
-
     def __fetch_uncles__(self, uncles: List[str], block_hash: str):
         blocks = []
         for index, uncle_hash in enumerate(uncles):
-            data = {"jsonrpc": "2.0", "method": "etd_getUncleByBlockHashAndIndex", "params": [block_hash, hex(index)]}
-            resp = requests.post(self.url, json=data)
+            data = {"jsonrpc": "2.0", "method": "eth_getUncleByBlockHashAndIndex", "params": [block_hash, hex(index)]}
+            resp = requests.request("POST", self.url, json=data, headers=self.headers)
             resp_data: Dict = resp.json()['result']
-            resp_data = self.__normalize_dict__(resp_data)
             blocks.append(resp_data)
 
         return blocks
--- tests/block_fetcher/test_fetch.py
+++ tests/block_fetcher/test_fetch.py
@@ -1,15 +1,64 @@
-class TestFetch:
+import json
+import os
+import unittest
+from unittest.mock import patch
+
+import httpretty
+from pymongo import MongoClient
+
+from block_fetcher.block_fetcher import BlockFetcher
+
+
+class TestFetch(unittest.TestCase):
+    mock_block_response = {
+        "result": {
+            "number": "0x0",
+            "hash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+            "parentHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+            "nonce": "0x0000000000000000",
+            "sha3Uncles": "0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347",
+            "transactions": [{
+                "hash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+                "nonce": "0x0",
+                "blockHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+                "blockNumber": "0x0",
+                "transactionIndex": "0x0",
+                "from": "0x0000000000000000000000000000000000000000",
+                "to": "0x0000000000000000000000000000000000000000",
+                "value": "0x0",
+                "gas": "0x0",
+                "gasPrice": "0x0",
+                "input": "0x",
+                "raw": "0x",
+                "r": "0x0",
+                "s": "0x0",
+                "v": "0x0",
+
+            }]
+        }
+    }
+
+    def setUp(self) -> None:
+        os.environ = {
+            "RPC_URL": "http://localhost:8545",
+            "DATABASE_URL": "mongodb://server:27017",
+        }
+
+    # @patch("pymongo.MongoClient")
+    @httpretty.activate(allow_net_connect=True, verbose=True)
     def test_fetch(self):
-        assert True
+        mock_block_number_response = {
+            "result": hex(20)
+        }
 
-    def test_fetch_with_block_hash(self):
-        assert True
+        def rpc_callback(request, uri, headers):
+            if request.body == b'{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":"1"}':
+                return 200, headers, json.dumps(self.mock_block_response)
+            else:
+                return 200, headers, json.dumps(mock_block_number_response)
 
-    def test_fetch_with_block_number(self):
-        assert True
-
-    def test_fetch_with_block_hash_and_block_number(self):
-        assert True
-
-    def test_fetch_with_block_hash_and_block_number_and_transaction_index(self):
-        assert True
+        httpretty.register_uri(httpretty.POST, "http://localhost:8545/", body=rpc_callback)
+        block_fetcher = BlockFetcher()
+        self.assertEqual(block_fetcher.url, "http://localhost:8545")
+        # block_fetcher.fetch_blocks()
+        # self.assertEqual(block_fetcher.latest_db_block_number, 20)
