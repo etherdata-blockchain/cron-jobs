ac016bd8257d63cd78d49d271f93e82e7aed21e6
sirily11 <sirily1997@gmail.com>
2022-06-25 19:36:10 +0800

    feat: ci

---
+++ tests/block_fetcher/test_fetch.py
@@ -0,0 +1,15 @@
+class TestFetch:
+    def test_fetch(self):
+        assert True
+
+    def test_fetch_with_block_hash(self):
+        assert True
+
+    def test_fetch_with_block_number(self):
+        assert True
+
+    def test_fetch_with_block_hash_and_block_number(self):
+        assert True
+
+    def test_fetch_with_block_hash_and_block_number_and_transaction_index(self):
+        assert True
