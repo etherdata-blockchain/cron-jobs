f43aff89b949a08ab6c69ff6d11c6ccfcbbb4b97
sirily11 <sirily1997@gmail.com>
2022-11-04 01:08:12 +0800

    feat: use async await for block fetcher

--- block_fetcher/block_fetcher.py
+++ block_fetcher/block_fetcher.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 import random
 import traceback
@@ -5,6 +6,7 @@ import uuid
 from typing import Dict, List, Tuple
 
 import certifi
+import httpx
 import requests
 from pymongo import MongoClient
 from pymongo.collection import Collection
@@ -33,8 +35,9 @@ class BlockFetcher:
         self.should_insert_contract = should_insert_contract
         self.should_start_from_beginning = should_start_from_beginning
         self.default_db = os.getenv("DEFAULT_DB", "etd")
+        self.batch_size = int(os.getenv("BATCH_SIZE", 10))
 
-    def __connect_db__(self):
+    async def __connect_db__(self):
         self.mongodb = MongoClient(self.db_endpoint, tlsCAFile=certifi.where())
         block_collection = self.mongodb[self.default_db].blocks
         transaction_collection = self.mongodb[self.default_db].transactions
@@ -53,7 +56,7 @@ class BlockFetcher:
             print(e)
             print("Indexes already created")
 
-    def __fetch_rpc_block_number__(self) -> int:
+    async def __fetch_rpc_block_number__(self) -> int:
         data = {
             "jsonrpc": "2.0",
             "method": "eth_blockNumber",
@@ -61,12 +64,12 @@ class BlockFetcher:
             "id": str(uuid.uuid4())
         }
 
-        response = requests.request("POST", self.url, headers=self.headers, json=data)
+        response = await httpx.AsyncClient().post(self.url, headers=self.headers, json=data)
         resp_data = response.json()
         block_num_in_hex = resp_data['result']
         return int(block_num_in_hex, 16)
 
-    def __fetch_db_block_number__(self) -> int:
+    async def __fetch_db_block_number__(self) -> int:
         db = self.mongodb[self.default_db]
         blocks_col: Collection = db.blocks
         highest_blocks = list(blocks_col.find({}).sort("numberInBase10", -1).limit(1))
@@ -74,7 +77,7 @@ class BlockFetcher:
             return 0
         return int(highest_blocks[0]['numberInBase10'])
 
-    def __fetch_block__(self, block_number: int) -> Tuple[List, List]:
+    async def __fetch_block__(self, block_number: int) -> Tuple[List, List]:
         """
         Fetch block
         :param block_number:
@@ -83,12 +86,12 @@ class BlockFetcher:
         hex_block_number = hex(block_number)
         data = {"jsonrpc": "2.0", "method": "eth_getBlockByNumber", "params": [hex_block_number, True],
                 "id": str(uuid.uuid4()), }
-        resp = requests.request("POST", self.url, json=data, headers=self.headers)
+        resp = await httpx.AsyncClient().post(self.url, headers=self.headers, json=data)
         block: Dict = resp.json()['result']
         block["numberInBase10"] = int(block["number"], 16)
         block["timestamp"] = int(block["timestamp"], 16)
         blocks = []
-        uncles = self.__fetch_uncles__(block['uncles'], block['hash'])
+        uncles = await self.__fetch_uncles__(block['uncles'], block['hash'])
         transactions = block.pop("transactions")
         # add timestamp to transactions
         for tx in transactions:
@@ -99,7 +102,7 @@ class BlockFetcher:
 
         return blocks, transactions
 
-    def __fetch_contract__(self, block_hash: str, transaction_address: str, byte_code: str) -> Contract:
+    async def __fetch_contract__(self, block_hash: str, transaction_address: str, byte_code: str) -> Contract:
         """
         Fetch contract using block hash and transaction address
         :param block_hash:
@@ -114,7 +117,7 @@ class BlockFetcher:
             "id": random.randint(1, 100000)
         }
 
-        response = requests.request("POST", self.url, headers=self.headers, json=data)
+        response = await httpx.AsyncClient().post(self.url, headers=self.headers, json=data)
         resp_data = response.json()['result']
         contract_address = resp_data['contractAddress']
         creator_address = resp_data['from']
@@ -122,24 +125,24 @@ class BlockFetcher:
                             byte_code=byte_code, transaction_hash=transaction_address)
         return contract
 
-    def __process_transactions__(self, transactions: List[Dict]):
+    async def __process_transactions__(self, transactions: List[Dict]):
         for tx in transactions:
             if tx['to'] is None:
                 # contract creation
-                contract = self.__fetch_contract__(tx['blockHash'], tx['hash'], tx['input'])
-                self.__insert_contract__(contract)
+                contract = await self.__fetch_contract__(tx['blockHash'], tx['hash'], tx['input'])
+                await self.__insert_contract__(contract)
 
-    def remove_blocks(self):
+    async def remove_blocks(self):
         db = self.mongodb[self.default_db]
         block_col: Collection = db.blocks
         block_col.delete_many({})
 
-    def __fetch_uncles__(self, uncles: List[str], block_hash: str):
+    async def __fetch_uncles__(self, uncles: List[str], block_hash: str):
         blocks = []
         for index, uncle_hash in enumerate(uncles):
             data = {"jsonrpc": "2.0", "method": "eth_getUncleByBlockHashAndIndex", "params": [block_hash, hex(index)],
                     "id": str(uuid.uuid4())}
-            resp = requests.request("POST", self.url, json=data, headers=self.headers)
+            resp = await httpx.AsyncClient().post(self.url, headers=self.headers, json=data)
             resp_data: Dict = resp.json()['result']
             resp_data["numberInBase10"] = int(resp_data["number"], 16)
             resp_data["isUncle"] = True
@@ -147,21 +150,21 @@ class BlockFetcher:
 
         return blocks
 
-    def __insert_block__(self, blocks):
+    async def __insert_block__(self, blocks):
         if not self.should_insert_block:
             return
         db = self.mongodb[self.default_db]
         blocks_col: Collection = db.blocks
         blocks_col.insert_many(blocks)
 
-    def __insert_transactions__(self, transactions):
+    async def __insert_transactions__(self, transactions):
         if not self.should_insert_transaction:
             return
         db = self.mongodb[self.default_db]
         tx_col: Collection = db.transactions
         tx_col.insert_many(transactions)
 
-    def __insert_contract__(self, contract: Contract):
+    async def __insert_contract__(self, contract: Contract):
         print(f"Inserting contract: {contract.address}")
         if not self.should_insert_contract:
             return
@@ -169,14 +172,14 @@ class BlockFetcher:
         contract_col: Collection = db.contracts
         contract_col.insert_one(contract.to_dict())
 
-    def fetch_blocks(self):
+    async def fetch_blocks(self):
         """
         Fetch blocks from the latest block in db to the latest block in rpc
         :return:
         """
-        self.__connect_db__()
-        self.start_block_number = self.__fetch_db_block_number__() if not self.should_start_from_beginning else 0
-        self.end_block_number = self.__fetch_rpc_block_number__()
+        await self.__connect_db__()
+        self.start_block_number = await self.__fetch_db_block_number__() if not self.should_start_from_beginning else 0
+        self.end_block_number = await self.__fetch_rpc_block_number__()
         print(
             f"Start Block Number: {self.start_block_number}, "
             f"End Block Number: {self.end_block_number}")
@@ -184,22 +187,26 @@ class BlockFetcher:
         if self.start_block_number > self.end_block_number:
             raise Exception("DB block number is greater than rpc")
 
-        number = 0
-        for block_number in tqdm(range(self.start_block_number + 1, self.end_block_number)):
-            number += 1
-            if number % 10 == 0:
-                print(f"Fetching block {block_number}...")
-            try:
-                blocks, transactions = self.__fetch_block__(block_number)
-                self.__insert_block__(blocks)
-                if len(transactions) > 0:
-                    self.__process_transactions__(transactions)
-                    self.__insert_transactions__(transactions)
-            except Exception as e:
-                print(traceback.format_exc())
-                print(e)
+        for block_number in tqdm(range(self.start_block_number + 1, self.end_block_number, self.batch_size)):
+            async def fetch(at_block_number):
+                try:
+                    blocks, transactions = await self.__fetch_block__(at_block_number)
+                    await self.__insert_block__(blocks)
+                    if len(transactions) > 0:
+                        await self.__process_transactions__(transactions)
+                        await self.__insert_transactions__(transactions)
+                except Exception as e:
+                    print(traceback.format_exc())
+                    print(e)
+
+            # put jobs into a list
+            await asyncio.gather(*[fetch(block_number + i) for i in range(self.batch_size)])
+
+
+async def main():
+    fetcher = BlockFetcher()
+    await fetcher.fetch_blocks()
 
 
 if __name__ == '__main__':
-    fetcher = BlockFetcher()
-    fetcher.fetch_blocks()
+    asyncio.run(main())
