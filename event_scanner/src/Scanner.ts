02d7e965350e2c12e48a6aeef8ccbd34002adb5b
sirily11 <sirily1997@gmail.com>
2022-11-12 13:05:46 +0800

    fix: abi event fetcher won't fetch events

--- event_scanner/src/Scanner.ts
+++ event_scanner/src/Scanner.ts
@@ -1,11 +1,13 @@
 import { ethers } from "ethers";
 import logger from "node-color-log";
-import { FoundEvent } from "./EventFinder";
+import { type } from "os";
+import { erc1155, erc20, erc721, erc777 } from "./abi";
+import { EventFinder, FoundEvent } from "./EventFinder";
 
 interface Props {
   provider: ethers.providers.Provider;
   contractAddress: string;
-  abi: any;
+  abis: ABI[];
 }
 
 interface Event {
@@ -24,72 +26,90 @@ interface Event {
   data: { name: string; value: string; indexed: boolean; type: string }[];
 }
 
+type ABI = { [key: string]: any }[];
+
 export class ContractScanner {
   provider: ethers.providers.Provider;
   contractAddress: string;
-  abi: any;
+  abis: ABI[];
 
-  constructor({ provider, contractAddress, abi }: Props) {
+  constructor({ provider, contractAddress, abis }: Props) {
     this.provider = provider;
     this.contractAddress = contractAddress;
-    this.abi = abi;
+    if (abis.length === 0) {
+      logger.warn("Contract without ABI, using default ABI");
+      this.abis = [erc20, erc721, erc777, erc1155];
+    } else {
+      logger.info("Using custom ABI");
+      this.abis = abis;
+    }
   }
 
   /**
    * Scan the blockchain for events emitted by the contract.
    */
-  async scan(from: number, to: number, events: FoundEvent[]): Promise<Event[]> {
-    if (this.abi === undefined || this.abi === null) {
-      logger.warn("Skipping contract without ABI");
-      return [];
-    }
-    const contract = new ethers.Contract(
-      this.contractAddress,
-      this.abi,
-      this.provider
-    );
-    let scannedEvents: ethers.Event[] = [];
-    for (const event of events) {
-      scannedEvents = scannedEvents.concat(
-        await contract.queryFilter(event.name, from, to)
-      );
-    }
+  async scan(from: number, to: number): Promise<Event[]> {
     const returnedEvents: Event[] = [];
-    let index = 0;
-    for (const event of scannedEvents) {
-      const {
-        blockNumber,
-        blockHash,
-        address,
-        transactionHash,
-        event: name,
-      } = event;
-      const args: any = event.args || {};
-      const block = await event.getBlock();
-      const transaction = await event.getTransaction();
-      const argsFiltered = this.filterArgs(events[index], args);
 
-      returnedEvents.push({
-        blockNumber: ethers.utils.hexValue(blockNumber),
-        blockHash,
-        blockTimestamp: ethers.utils.hexValue(block.timestamp),
-        address,
-        transaction: {
-          hash: transactionHash,
-          index: ethers.utils.hexValue(event.transactionIndex),
-          from: transaction.from,
-          to: transaction.to!,
-          value: transaction.value._hex,
-        },
-        event: name || "",
-        data: argsFiltered,
-      });
+    for (const abi of this.abis) {
+      const contract = new ethers.Contract(
+        this.contractAddress,
+        abi,
+        this.provider
+      );
+      let scannedEvents: ethers.Event[] = [];
+      const eventFinder = new EventFinder(abi);
+      const events = eventFinder.findEvents();
+      for (const event of events) {
+        scannedEvents = scannedEvents.concat(
+          await contract.queryFilter(event.name, from, to)
+        );
+      }
+
+      for (const event of scannedEvents) {
+        const {
+          blockNumber,
+          blockHash,
+          address,
+          transactionHash,
+          event: name,
+        } = event;
+        const args: any = event.args || {};
+        if (args === undefined || args === null) {
+          continue;
+        }
+        const block = await event.getBlock();
+        const transaction = await event.getTransaction();
+
+        for (const foundEvent of events) {
+          const argsFiltered = this.filterArgs(foundEvent, args);
+          if (argsFiltered.length === 0) {
+            continue;
+          }
+
+          returnedEvents.push({
+            blockNumber: ethers.utils.hexValue(blockNumber),
+            blockHash,
+            blockTimestamp: ethers.utils.hexValue(block.timestamp),
+            address,
+            transaction: {
+              hash: transactionHash,
+              index: ethers.utils.hexValue(event.transactionIndex),
+              from: transaction.from,
+              to: transaction.to!,
+              value: transaction.value._hex,
+            },
+            event: name || "",
+            data: argsFiltered,
+          });
+        }
+      }
     }
 
     return returnedEvents;
   }
 
-  private filterArgs(foundEvent: FoundEvent, args: ethers.utils.Result): any {
+  private filterArgs(foundEvent: FoundEvent, args: ethers.utils.Result): any[] {
     const returnedArgs = [];
     let index = 0;
     if (Object.keys(args).length === 0) {
@@ -99,6 +119,10 @@ export class ContractScanner {
     for (const input of foundEvent.inputs) {
       const { name, type, indexed } = input;
       let value = args[index];
+      if (value === undefined) {
+        index++;
+        continue;
+      }
       if (value._hex !== undefined) {
         value = value._hex;
       }
